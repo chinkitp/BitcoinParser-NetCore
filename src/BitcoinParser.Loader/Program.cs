99155a3521475b43070653881d4fdc0fd6785cc6
Chinkit Patel <chinkit@savebigonfuel.com.au>
2018-02-02 19:06:23 +1030

    loading into the db

--- src/BitcoinParser.Loader/Program.cs
+++ src/BitcoinParser.Loader/Program.cs
@@ -1,6 +1,10 @@
 ﻿using BitcoinBlockchain.Data;
 using BitcoinBlockchain.Parser;
 using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace BitcoinParser.Loader
 {
@@ -9,19 +13,35 @@ namespace BitcoinParser.Loader
         static void Main(string[] args)
         {
             IBlockchainParser blockchainParser = new BlockchainParser("./../../../../Samples/");
-            foreach (Block block in blockchainParser.ParseBlockchain())
+            var blocks = blockchainParser.ParseBlockchain();
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            for (int i = 0; i < blocks.Count() % 1000 ; i++)
             {
-                using(var bContext = new BitcoinDbContext())
+                var subSet = blocks.Skip(i * 1000).Take(1000);
+                var blocksToAdd = new List<BitcoinDataLayerAdoNet.Data.Block>();
+
+                foreach (var item in subSet)
                 {
-                    bContext.Block.Add(new BitcoinDataLayerAdoNet.Data.Block(
-                        (int) block.BlockHeader.BlockVersion,
-                        block.BlockHeader.BlockHash.ToArray(),
-                        block.BlockHeader.PreviousBlockHash.ToArray(),
-                        block.BlockHeader.BlockTimestamp
-                        ));
+                    blocksToAdd.Add(new BitcoinDataLayerAdoNet.Data.Block(
+                        (int)item.BlockHeader.BlockVersion,
+                        item.BlockHeader.BlockHash.ToArray(),
+                        item.BlockHeader.PreviousBlockHash.ToArray(),
+                        item.BlockHeader.BlockTimestamp));
+                }
+
+                using (var bContext = new BitcoinDbContext())
+                {
+                    bContext.Block.AddRange(blocksToAdd);
                     bContext.SaveChanges();
-                }              
+                }
             }
+
+
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds);
+
         }
     }
 }
